593368ff3f5615550b7f7459127537ae484db7e3
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-02-17 18:51:46 +0200

    added task3

--- task_03.py
+++ task_03.py
@@ -1,12 +1,27 @@
-def print_figure(number, symbol):
+def print_square_v01(number, symbol):
     print()
-    num = round(number / 2)
     for i in range(number):
         line = " ".join([symbol] * round(number / 2))
-        print(f"{line}{""if number % 2 == 0 else " "}")
+        print(f"{symbol if number == 1 else line}{"" if number % 2 != 0 else " "}")
+
+
+def print_square_v02(number, symbol):
+    besa = [symbol, " "]
+    line = create_line(besa,input_number)
+
+    print(f"{line}\n" * number)
+
+
+def create_line(besa, number):
+    line = ""
+    for i in range(number):
+        line += besa[i % 2]
+    return line
 
 
 input_number = int(input("Enter a number: "))
 input_symbol = input("Enter a symbol: ")
 
-print_figure(input_number, input_symbol)
+print_square_v01(input_number, input_symbol)
+print("\nV02\n")
+print_square_v02(input_number, input_symbol)
